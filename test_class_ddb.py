e6bcd177864f28e0766999c125c36d5a4fe48897
Sebastian Benavente <s.benaventebravo@gmail.com>
2020-11-19 18:24:48 -0300

    fix code style

--- test_class_ddb.py
+++ test_class_ddb.py
@@ -93,14 +93,14 @@ class TestClassDDB:
             client = AWSDynamoDB()
             client.set_table_name("Table")
             expected = client.table_exist()
-            assert expected == True
+            assert expected is True
 
     def test_table_not_exist(self, dynamodb_client):
         with ddb_table_setup(dynamodb_client):
             client = AWSDynamoDB()
             client.set_table_name("no existing table")
             expected = client.table_exist()
-            assert expected == False
+            assert expected is False
 
     def test_put_item_success(self, dynamodb_client):
         with ddb_table_setup(dynamodb_client):
@@ -113,25 +113,42 @@ class TestClassDDB:
             client = AWSDynamoDB()
             client.set_table_name("Table")
             expected = client.put_item(data_input)
-            assert expected == {"status": "success", "item": data_input}
+            assert expected == {
+                "status": "success",
+                "item": data_input
+            }
 
     def test_put_item_fail(self, dynamodb_client):
         with ddb_table_setup(dynamodb_client):
             data_input = {
                 "fail_schema": "no data schema"
-            } 
-
+            }
             client = AWSDynamoDB()
             client.set_table_name("Table")
             expected = client.put_item(data_input)
-            assert expected ==  {'status': 'error', 'message_error': 'An error occurred (ValidationException) when calling the PutItem operation: One or more parameter values were invalid: Missing the key key in the item'}
+            assert expected == {
+                'status': 'error',
+                'message_error': (
+                    'An error occurred (ValidationException)'
+                    ' when calling the PutItem operation:'
+                    ' One or more parameter values were invalid:'
+                    ' Missing the key key in the item'
+                )
+            }
 
     def test_get_item_success_item_found(self, dynamodb_client):
         with ddb_table_with_previous_data(dynamodb_client):
             client = AWSDynamoDB()
             client.set_table_name("Table")
             expected = client.get_item({"key": "key", "sort": "sort"})
-            assert expected == {'item': {'data': {}, 'key': 'key', 'sort': 'sort'}, 'status': 'success'}
+            assert expected == {
+                'item': {
+                    'data': {},
+                    'key': 'key',
+                    'sort': 'sort'
+                },
+                'status': 'success'
+            }
 
     def test_get_item_success_item_not_found(self, dynamodb_client):
         with ddb_table_with_previous_data(dynamodb_client):
@@ -145,4 +162,11 @@ class TestClassDDB:
             client = AWSDynamoDB()
             client.set_table_name("Table")
             expected = client.get_item({"bad schema": "no value"})
-            assert expected == {'status': 'error', 'message_error': 'An error occurred (ValidationException) when calling the GetItem operation: Validation Exception'}
+            assert expected == {
+                'status': 'error',
+                'message_error': (
+                    'An error occurred (ValidationException)'
+                    ' when calling the GetItem operation:'
+                    ' Validation Exception'
+                )
+            }
