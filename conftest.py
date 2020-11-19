bc598d825ce28c0eb269b708df5b63cd8b70064e
Sebastian Benavente <s.benaventebravo@gmail.com>
2020-11-19 17:31:46 -0300

    add environ variable AWS_REGION

--- conftest.py
+++ conftest.py
@@ -15,5 +15,5 @@ def aws_credentials():
 def dynamodb_client(aws_credentials):
     """DDB mock client"""
     with mock_dynamodb2():
-        conn = boto3.resource('dynamodb')
+        conn = boto3.resource('dynamodb', region_name="testing_region")
         yield conn
